6a0c25c023cf998a742da745f3a1badef45ff47e
RaelReis <rafaelxdr1xdr1@gmail.com>
2023-08-18 17:29:29 -0300

    adding sameSite: none in cookies

--- src/server.ts
+++ src/server.ts
@@ -92,7 +92,7 @@ app.get("/auth/discord/callback", async (req, res) => {
 
   const token = await sign({ sub: id }, process.env.JWT_SECRET as string, { expiresIn: "7d" });
 
-  res.cookie("token", `Bearer ${token}`, { sameSite: "none" });
+  res.cookie("token", `Bearer ${token}`, { sameSite: "none", secure: true });
   res.redirect(301, process.env.CLIENT_REDIRECT_URL || "");
 });
 
